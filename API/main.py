12f0df79e63df60fb505cfe928ed34ba790d2874
AbdurRafaybahria <rafayabdur969@gmail.com>
2025-09-26 03:46:42 -0700

    Update CORS configuration to allow Vercel frontend and localhost

--- API/main.py
+++ API/main.py
@@ -50,9 +50,15 @@ DEFAULT_CMS_URL = os.getenv("REACT_APP_BASE_URL", "http://localhost:3000")
 app = FastAPI(title="Process Optimization API", version="1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://fyp-cms-frontend.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
